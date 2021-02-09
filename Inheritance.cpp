6165b6a6555e6d5fadf950a48deff1ebd3e9f2dc
ianlin1102 <73145204+ianlin1102@users.noreply.github.com>
2021-02-09 17:59:21 -0600

    f
    hello

---
+++ Inheritance.cpp
@@ -0,0 +1,36 @@
+﻿#include "pch.h"
+#include <iostream>
+#include <string>
+#include <math.h>
+using namespace std;
+
+//class base
+//{
+//public:
+//	 base()
+//	{
+//		m_A = 100;
+//	}
+//	int m_A;
+//};
+//class son : public base
+//{
+//public:
+//	son()
+//	{
+//		m_A = 200;//同名与base 的m_A
+//	}
+//	int m_A;
+//};
+//void test01()
+//{
+//	son a;
+//	cout << "son's m_A =" << a.m_A << endl;//son的m_A
+//	cout << "base's m_A =" << a.base::m_A << endl;
+//}
+//int main()
+//{
+//	test01();
+//	system("pause");
+//    std::cout << "Hello World!\n"; 
+//}
